40788650de6e41ffa38da0093dbdff5865d0b3f4
LawrZhu <lawrencexzhu@gmail.com>
2016-09-14 13:04:09 -0700

    ChexTex
    Ver. 0.01

---
+++ app/src/main/java/com/chextex/chextex/chextex/ContactList.java
@@ -0,0 +1,15 @@
+package com.chextex.chextex.chextex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactList extends AppCompatActivity {
+
+    //TODO: make it scroll
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+    }
+}
