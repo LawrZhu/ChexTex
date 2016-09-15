e3bb5d30db54d9a7fe98543112ace1ea49881e79
LawrZhu <lawrencexzhu@gmail.com>
2016-09-15 16:00:01 -0700

    Added contact constructor, and get/set functions

--- app/src/main/java/com/chextex/chextex/chextex/Contact.java
+++ app/src/main/java/com/chextex/chextex/chextex/Contact.java
@@ -5,4 +5,37 @@ package com.chextex.chextex.chextex;
  */
 public class Contact {
 
+    protected String name = "";
+    protected String phoneNumber = "";
+    protected String address = "";
+
+    public Contact(String name, String phoneNumber, String address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
