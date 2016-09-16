225bbb09769003a8ba6ac6df958674d01d261db0
LawrZhu <lawrencexzhu@gmail.com>
2016-09-15 18:57:35 -0700

    working contact list

--- app/src/main/java/com/chextex/chextex/chextex/ContactList.java
+++ app/src/main/java/com/chextex/chextex/chextex/ContactList.java
@@ -2,35 +2,56 @@ package com.chextex.chextex.chextex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ContactList extends AppCompatActivity {
     ListView listView;
-    ArrayList<Contact> myContacts = new ArrayList<Contact>();
-    ArrayAdapter adapter;
+    List<Contact> myContacts = new ArrayList<Contact>();
+    ArrayAdapter<Contact> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+        listView = (ListView) findViewById(R.id.listView);
+        createFakeContacts();
 
-        listView = (ListView)findViewById(R.id.listView);
+        adapter = new contactListAdapter();
+        listView.setAdapter(adapter);
 
     }
 
+    //Tests to make fake contacts
     private void createFakeContacts() {
-        Contact testContact = new Contact("lawrence","911","address");
-        Contact testContact2 = new Contact("michael","415","2nd address");
-        myContacts.add(testContact);
-        myContacts.add(testContact2);
+        myContacts.add(new Contact("lawrence","911","address"));
+        myContacts.add(new Contact("michael","415","2nd address"));
     }
 
     private class contactListAdapter extends ArrayAdapter<Contact> {
         public contactListAdapter() {
-            super(ContactList.this,R.layout.activity_contact_list, myContacts);
+            super(ContactList.this,R.layout.listview_item, myContacts);
+        }
+
+        @Override
+        public View getView(int position,View view, ViewGroup parent) {
+            if (view == null)
+                view = getLayoutInflater().inflate(R.layout.listview_item, parent, false);
+            Contact cur = myContacts.get(position);
+            TextView name = (TextView) view.findViewById(R.id.contact_name);
+            name.setText(cur.getName());
+            TextView address = (TextView) view.findViewById(R.id.contact_address);
+            address.setText(cur.getAddress());
+            TextView phone = (TextView) view.findViewById(R.id.contact_number);
+            phone.setText(cur.getPhoneNumber());
+
+            return view;
         }
     }
 }
