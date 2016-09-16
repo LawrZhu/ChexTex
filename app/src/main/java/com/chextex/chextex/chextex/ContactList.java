08b75b95622d9c23a8bd4e793e1756f91fbed24a
michaeldiep <michaelthanhdiep@gmail.com>
2016-09-15 21:40:57 -0700

    made the click for you

--- app/src/main/java/com/chextex/chextex/chextex/ContactList.java
+++ app/src/main/java/com/chextex/chextex/chextex/ContactList.java
@@ -3,12 +3,14 @@ package com.chextex.chextex.chextex;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +26,16 @@ public class ContactList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
         listView = (ListView) findViewById(R.id.listView);
+
+        listView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: LAWRENCE ADD YOUR CODE HERE
+                Toast.makeText(ContactList.this, "I HAVE A SMALL PENIS",
+                        Toast.LENGTH_LONG).show();
+                Log.d("M","SMALLPENIS");
+            }
+        });
 
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
