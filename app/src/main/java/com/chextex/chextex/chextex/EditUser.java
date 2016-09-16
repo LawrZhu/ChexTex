8e8fda28a07340e4b8d85f4510ec5ee24947e7cf
LawrZhu <lawrencexzhu@gmail.com>
2016-09-15 23:11:13 -0700

    something

--- app/src/main/java/com/chextex/chextex/chextex/EditUser.java
+++ app/src/main/java/com/chextex/chextex/chextex/EditUser.java
@@ -1,5 +1,6 @@
 package com.chextex.chextex.chextex;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,7 +11,6 @@ import android.widget.EditText;
 public class EditUser extends AppCompatActivity {
 
     protected Button confirm;
-    protected Bundle bundle;
     protected String name;
     protected String phoneNumber;
     protected String address;
@@ -21,11 +21,8 @@ public class EditUser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user);
 
-        bundle = getIntent().getExtras();
-        name = bundle.getString("name");
-        phoneNumber = bundle.getString("phoneNumber");
-        address = bundle.getString("address");
-        contactIndex = bundle.getInt("index");
+        SharedPreferences sp = getSharedPreferences("Contacts", Context.MODE_PRIVATE);
+
 
         EditText n = (EditText)findViewById(R.id.name);
         EditText pa = (EditText)findViewById(R.id.postalAddress);
@@ -40,17 +37,8 @@ public class EditUser extends AppCompatActivity {
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(EditUser.this, ContactList.class);
 
-                bundle = new Bundle();
-                bundle.putString("name", name);
-                bundle.putString("address", address);
-                bundle.putString("phoneNumber", phoneNumber);
-                bundle.putInt("index", contactIndex);
-
-                i.putExtras(bundle);
-
-                startActivity(i);
+            startActivity(new Intent(EditUser.this, ContactList.class));
             }
         });
     }
